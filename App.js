a08c2b06251ed4b89d0066b06f8e96a2340d3ec4
ZuzaKantorek <zuzannakantorek999@gmail.com>
2019-08-08 18:00:23 +0200

    moj pierwszy commit

---
+++ App.js
@@ -0,0 +1,49 @@
+/**
+ * Sample React Native App
+ * https://github.com/facebook/react-native
+ *
+ * @format
+ * @flow
+ */
+
+import React, {Fragment} from 'react';
+import {
+  SafeAreaView,
+  StyleSheet,
+  ScrollView,
+  View,
+  Text,
+  StatusBar,
+} from 'react-native';
+
+import {
+  Header,
+  LearnMoreLinks,
+  Colors,
+  DebugInstructions,
+  ReloadInstructions,
+} from 'react-native/Libraries/NewAppScreen';
+
+const App = () => {
+  return (
+    <View>
+      <StatusBar barStyle="dark-content" />
+ 
+        <ScrollView
+          contentInsetAdjustmentBehavior="automatic"
+          style={styles.scrollView}>
+          <Text>Czesc tu Zuza</Text>
+        </ScrollView>
+     
+    </View>
+  );
+};
+
+const styles = StyleSheet.create({
+  scrollView: {
+    backgroundColor: Colors.lighter,
+  },
+ 
+});
+
+export default App;
